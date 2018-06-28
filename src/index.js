8cc2da4663557b266cf05cb23fb778651be9e033
Marton Papp <marton.papp@emarsys.com>
2018-06-28 11:14:15 +0200

    feat(snippet): track first steps SUITEDEV-13251

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello Magento2!');
+'use strict';
+
+Emarsys = Emarsys || {};
+Emarsys.Magento2 = Emarsys.Magento2 || {};
+Emarsys.Magento2.track = function() {
+  window.require(['Magento_Customer/js/customer-data'], function(customerData) {
+    console.log(customerData);
+  });
+};
