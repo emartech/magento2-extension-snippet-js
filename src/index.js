a176f975ac147dd45e6f25525ead3e4ab66a80da
Marton Papp <marton.papp@emarsys.com>
2018-06-28 12:37:18 +0200

    feat(tests): start from a clean state by deleting from cache SUITEDEV-13251

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-Emarsys = Emarsys || {};
-Emarsys.Magento2 = Emarsys.Magento2 || {};
-Emarsys.Magento2.track = function() {
+window.Emarsys = window.Emarsys || {};
+window.Emarsys.Magento2 = window.Emarsys.Magento2 || {};
+window.Emarsys.Magento2.track = function() {
   window.require(['Magento_Customer/js/customer-data'], function(customerData) {
     console.log(customerData);
   });
--- src/index.spec.js
+++ src/index.spec.js
@@ -5,8 +5,30 @@ const sinonChai = require('sinon-chai');
 const expect = chai.expect;
 chai.use(sinonChai);
 
+const requireUncached = (module) => {
+  delete require.cache[require.resolve(module)];
+  return require(module);
+};
+
+const setupSnippet = customerData => {
+  global.window = {
+    require: (arr, callback) => {
+      callback(customerData);
+    }
+  };
+  requireUncached('./');
+};
+
 describe('Magento2 Extension', function() {
-  it('should work', function() {
-    expect(true).to.be.true;
+  it('should register a track function', function() {
+    setupSnippet({ emarsys: 'magento' });
+    global.window.Emarsys.Magento2.track();
+    expect(global.window.Emarsys.Magento2.track).to.be.a('function');
+  });
+
+  it('should work multiple times', function() {
+    setupSnippet({ emarsys: 'shopify' });
+    global.window.Emarsys.Magento2.track();
+    expect(global.window.Emarsys.Magento2.track).to.be.a('function');
   });
 });
