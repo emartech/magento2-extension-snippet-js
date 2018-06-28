454fe552f50ae2ba83f70d306ee954da75dff8b0
Marton Papp <marton.papp@emarsys.com>
2018-06-28 10:48:40 +0200

    feat(deploy): add mocha and test SUITEDEV-13251

---
+++ src/index.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
+chai.use(sinonChai);
+
+describe('Magento2 Extension', function() {
+  it('should work', function() {
+    expect(true).to.be.true;
+  });
+});
